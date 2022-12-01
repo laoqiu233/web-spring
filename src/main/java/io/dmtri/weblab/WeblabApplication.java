42a6ea9443316a90bbc1151c5d16b396b41c4739
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2022-12-01 12:48:04 +0300

    Init

---
+++ src/main/java/io/dmtri/weblab/WeblabApplication.java
@@ -0,0 +1,13 @@
+package io.dmtri.weblab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dmtri/weblab/WeblabApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dmtri.weblab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
