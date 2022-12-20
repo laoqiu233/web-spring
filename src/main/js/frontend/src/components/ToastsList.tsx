fd2f886141681e5479a4fbbdd44bc625b7390108
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2022-12-21 01:47:51 +0300

    Added timeouts for toasts

--- src/main/js/frontend/src/components/ToastsList.tsx
+++ src/main/js/frontend/src/components/ToastsList.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react"
 import { CSSTransition, TransitionGroup } from "react-transition-group"
-import { Toast } from "../features/toasts/toastsSlice"
+import { setTokenSourceMapRange } from "typescript"
+import { useAppDispatch } from "../app/hooks"
+import { Toast, toastRemoved } from "../features/toasts/toastsSlice"
 
 interface ToastsListProps {
     toasts: Toast[],
+    onToastClose(toastId: number): void
+};
+
+interface ToastWidgetProps {
+    toast: Toast,
     onToastClose(toastId: number): void
 };
 
@@ -31,20 +39,39 @@ const iconsForToastType = {
 }
 
 
+function ToastWidget({toast, onToastClose} : ToastWidgetProps) {
+    const dispatch = useAppDispatch();
+
+    // Start a timer when the toast is mounted
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            dispatch(toastRemoved(toast.id));
+        }, 5000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        }
+    }, [dispatch, toast]);
+    
+    return (
+        <div className={`${backgroundColorForToastType[toast.type]} ${borderColorForToastType[toast.type]} border-2 relative p-3 mt-3 text-white shadow-lg rounded-xl md:w-full`}>
+            <h1 className="text-lg font-bold"><i className={iconsForToastType[toast.type]}></i>  {titleForToastType[toast.type]}</h1>
+            <p>{toast.message}</p>
+            <i 
+                className="bi bi-x-circle absolute top-2 right-2 hover:cursor-pointer hover:text-gray-300"
+                onClick={() => onToastClose(toast.id)}
+            ></i>
+        </div>
+    )
+}
+
 export default function ToastsList({toasts, onToastClose}: ToastsListProps) {
     return (
         <TransitionGroup className='fixed bottom-5 left-2 right-2 md:right-auto md:max-w-xs md:w-full'>
         {
             toasts.map((v) => (
                 <CSSTransition key={v.id} classNames='toast-fade' timeout={200}>
-                    <div className={`${backgroundColorForToastType[v.type]} ${borderColorForToastType[v.type]} border-2 relative p-3 mt-3 text-white shadow-lg rounded-xl md:w-full`}>
-                        <h1 className="text-lg font-bold"><i className={iconsForToastType[v.type]}></i>  {titleForToastType[v.type]}</h1>
-                        <p>{v.message}</p>
-                        <i 
-                            className="bi bi-x-circle absolute top-2 right-2 hover:cursor-pointer hover:text-gray-300"
-                            onClick={() => onToastClose(v.id)}
-                        ></i>
-                    </div>
+                    <ToastWidget toast={v} onToastClose={onToastClose} />
                 </CSSTransition>
             ))
         }
